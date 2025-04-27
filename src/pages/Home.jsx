1a9e86a5c019380d24665e4a11771a584a45a008
UnluckyPants <franklindu8@gmail.com>
2025-04-27 15:28:20 -0700

    Fixed footer and subfooter, added subheader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import styles from '../static/Home.module.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Subfooter from '../components/Subfooter'
+import Subheader from '../components/Subheader'
 
 export default function Home() {
 
   return (
     <>
+        <Subheader/>
         <Header />
         <div className= {styles.body}><h1>this is home</h1></div>
         <div style={{height: "1000px", background: "lightblue"}}></div>
